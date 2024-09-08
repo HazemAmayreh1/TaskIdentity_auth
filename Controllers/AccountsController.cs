3d90cba6dee101fa8fbd5782dbee6f85a83c59df
HazemAmayreh1 <hazemamayreh44@gmail.com>
2024-09-08 20:07:45 +0300

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskIdentity.Data;
+using TaskIdentity.Models.ViewModel;
+
+namespace TaskIdentity.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signinManger;
+
+        public AccountsController(ApplicationDbContext context,UserManager<IdentityUser>userManager,SignInManager<IdentityUser> signinManger) 
+        {
+            this.context = context;
+            this.userManager = userManager;
+            this.signinManger = signinManger;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            IdentityUser user = new IdentityUser()
+            {
+                Email = model.Email,
+                PhoneNumber = model.Phone,
+                UserName = model.Email,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return View(model);
+            }
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async  Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await signinManger.PasswordSignInAsync(model.Email,model.Password,model.RememberMe,false);
+
+            if (result.Succeeded) {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+           
+        }
+
+    }
+}
---
+++ Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskIdentity.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(50)]
+        public string Email { get; set; }  
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20)]
+
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Phone { get; set; }
+
+
+    }
+}
